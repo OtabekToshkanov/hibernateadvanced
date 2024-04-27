6167a4c950db33649d49c5f22fff6d17b63f6d14
Otabek Toshkanov <toshkanov.otabek@gmail.com>
2024-04-27 14:16:18 +0500

    Create user with roles

--- src/main/java/uz/smartup/academy/hibernateadvanced/HibernateadvancedApplication.java
+++ src/main/java/uz/smartup/academy/hibernateadvanced/HibernateadvancedApplication.java
@@ -1,25 +1,44 @@
 package uz.smartup.academy.hibernateadvanced;
 
-import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
-import uz.smartup.academy.hibernateadvanced.dao.AppDAO;
-import uz.smartup.academy.hibernateadvanced.entity.Course;
-import uz.smartup.academy.hibernateadvanced.entity.Instructor;
-import uz.smartup.academy.hibernateadvanced.entity.Student;
+import uz.smartup.academy.hibernateadvanced.entity.*;
+import uz.smartup.academy.hibernateadvanced.service.UserService;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @SpringBootApplication
-public class HibernateadvancedApplication extends SpringBootServletInitializer {
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(HibernateadvancedApplication.class);
-    }
+public class HibernateadvancedApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HibernateadvancedApplication.class, args);
+    }
+
+    @Bean
+    public ApplicationRunner runner(UserService userService) {
+        return args -> {
+            User user = new User();
+            user.setId(6);
+            user.setUsername("otabek");
+            user.setPassword("1234");
+            user.setEnabled("Y");
+            user.setFirstName("Otabek");
+            user.setLastName("Toshkanov");
+            user.setEmail("otabek@mail");
+
+            Set<Role> roles = new HashSet<>();
+            Role role = new Role();
+            role.setRole("ADMIN");
+            roles.add(role);
+            Role role2 = new Role();
+            role2.setRole("STUDENT");
+            roles.add(role2);
+
+            userService.registerUser(user, roles);
+        };
     }
 
 //    @Bean
